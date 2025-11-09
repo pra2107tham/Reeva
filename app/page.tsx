17596d36526b0728f8b8305b065bc58ac88cf01c
pra2107tham <prathamshirbhate1909@gmail.com>
2025-11-09 21:54:18 +0530

    Enable React strict mode in Next.js configuration; enhance Home component with Hero and Background framer components for improved visual presentation.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,25 @@
 "use client";
+import Hero from "@/framer/hero";
+import Background from "@/framer/background";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Hero.Responsive style={{ width: "100%" }} />
+      <Background.Responsive
+        style={{
+          position: "absolute",
+          bottom: "5210px",
+          left: "0px",
+          right: "0px",
+          top: "0px",
+          maxWidth: "1480px",
+          zIndex: -1,
+          pointerEvents: "none",
+          width: "100%",
+        }}
+      />
+      {/* <WhyHow.Responsive style={{ width: "100%" }} /> */}
     </div>
   );
 }
